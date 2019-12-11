2e12a910df43560acaa7b397edd3a3d7511aada9
AidenTran15 <aidenkiettran@gmail.com>
2019-12-11 14:28:03 +1100

    [Update] Set up index

--- calc/urls.py
+++ calc/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from. import views
 
-urlpatterns = [path("",views.home, name="home"),path("add",views.add, name="add")]
+urlpatterns = [
+    path("",views.index, name="index"),
+    path("add",views.add, name="add")
+    ]
