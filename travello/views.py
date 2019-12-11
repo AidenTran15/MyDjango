43044e29d5e793d87564fa682e067efa0f3a59a3
AidenTran15 <aidenkiettran@gmail.com>
2019-12-11 22:56:14 +1100

    [Add New] Create a class destination

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import destination
 
 # Create your views here.
 
 def index(request):
-    return render (request, "index.html")
+
+    dest1 = destination() 
+
+    return render (request, "index.html", {'dest1': dest1})
+
