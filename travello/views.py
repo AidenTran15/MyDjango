247c122747764640bc1e1693d86d53fb36eaaa2c
AidenTran15 <aidenkiettran@gmail.com>
2019-12-12 00:50:31 +1100

    [AddNew] Create des1

--- travello/views.py
+++ travello/views.py
@@ -9,6 +9,9 @@ def index(request):
     dest1.name = 'Munbai'
     dest1.desc = 'The City That Never Sleeps'
     dest1.price = 700
+    dest1.img = 'destination_1.jgp'
+
+    dest2 =  destination()
 
     return render (request, "index.html", {'dest1': dest1})
 
