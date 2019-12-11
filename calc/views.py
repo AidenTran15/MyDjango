e2041403b213986596ee43bad2886f74155db882
AidenTran15 <aidenkiettran@gmail.com>
2019-12-11 13:42:21 +1100

    [AddNew] Create new app with name travello

--- calc/views.py
+++ calc/views.py
@@ -7,10 +7,7 @@ def home(request):
     return render(request, "home.html")
 
 def add(request):
-
-    val1 = int(request.POST['num1'])
-    val2 = int(request.POST['num2'])
-    res = val1 + val2
-
-
+    val1 = request.GET["num1"]
+    val2 = request.GET["num2"]
+    res  = val1 + val2
     return render(request, 'result.html', {'result': res})
