ab393057f98f13c14a3ba01190f24c0c7cfb374d
AidenTran15 <aidenkiettran@gmail.com>
2019-12-12 00:40:06 +1100

    [Update] Create a new destination

--- travello/views.py
+++ travello/views.py
@@ -8,6 +8,7 @@ def index(request):
     dest1 = destination() 
     dest1.name = 'Munbai'
     dest1.desc = 'The City That Never Sleeps'
+    dest1.price = 700
 
     return render (request, "index.html", {'dest1': dest1})
 
