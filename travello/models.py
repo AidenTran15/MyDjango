3e53b0e1997ec718d12c4372b6aab3d8ac58cf80
AidenTran15 <aidenkiettran@gmail.com>
2019-12-12 14:38:35 +1100

    [Update] put dest.offer into view.py

--- travello/models.py
+++ travello/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class destination():
-    name = str
+    id : int
+    name : str
+    img : str
+    desc : str
+    price : int
+    offer : bool
--- travello/views.py
+++ travello/views.py
@@ -10,18 +10,21 @@ def index(request):
     dest1.desc = 'The City That Never Sleeps'
     dest1.price = 700
     dest1.img = 'destination_1.jgp'
+    dest1.offer = False
 
     dest2 =  destination()
     dest2.name = 'Vung Tau'
     dest2.desc = 'Thanh Pho Bien'
     dest2.price = 300
     dest2.img = 'destination_2.jgp'
+    dest2.offer = True
 
     dest3 = destination()
     dest3.name = 'Da Lat'
     dest3.desc = 'Thanh Pho Suong Mu'
     dest3.price = 200
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
