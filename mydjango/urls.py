96d10a979ff8662b2193caa374738ff99b643819
AidenTran15 <aidenkiettran@gmail.com>
2019-12-13 12:19:46 +1100

    [Update] Create a account app

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
-    
+    path('accounts/', include('accounts.urls'))
 ]
 
 utlpattern = urlpatterns + static(settings.Media_URL, document_root=settings.Media)
