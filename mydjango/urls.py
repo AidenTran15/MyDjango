089b1ee8dccd12ffc9a6b7892db526bfb410a0bb
AidenTran15 <aidenkiettran@gmail.com>
2019-12-13 11:57:42 +1100

    [AddNew] Create the superuser

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
     
 ]
+
+utlpattern = urlpatterns + static(settings.Media_URL, document_root=settings.Media)
